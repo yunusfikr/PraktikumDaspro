10e905768394f7c6b9a8892c993db4ecb7320173
yunusfikr <yunusfikr07@gmail.com>
2025-10-01 10:57:34 +0700

    percobaan 1

---
+++ jobsheet5/ifCetakKRS21.java
@@ -0,0 +1,22 @@
+package jobsheet5;
+import java.util.Scanner; 
+
+class ifCetakKRS21 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        boolean uktlunas;
+
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas? (true/false): ");
+        uktlunas = sc.nextBoolean();
+
+        if (uktlunas){
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("silahkan cetak KRS dan minta tanda tangan DPA");
+        }
+        
+    }
+
+}
