b3d77ad0f246c5485d2e08ee120f836b2538b783
yunusfikr <yunusfikr07@gmail.com>
2025-11-26 10:55:34 +0700

    1

---
+++ pertemuan14/kafe21.java
@@ -0,0 +1,18 @@
+package pertemuan14;
+
+public class kafe21 {
+    public static void Menu() {
+        System.out.println("===== MENU RESTO KAFE =====");
+        System.out.println("1. Kopi HItam - Rp 15,000");
+        System.out.println("2. Cappuccino - Rp 20,000");
+        System.out.println("3. Latte - Rp 22,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
+        System.out.println("============================");
+        System.out.println("Silahkan pilih menu yang Anda inginkan");
+    }
+    public static void main(String[] args) {
+        Menu();
+    }
+}
